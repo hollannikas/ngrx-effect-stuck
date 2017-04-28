e348df0ae937822928eb0a1fcda45e9f8314ed4f
Julien <pro.julien@runbox.com>
2017-04-28 16:34:37 +0300

    actual issue reproduced

--- src/app/service.ts
+++ src/app/service.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import 'rxjs/add/observable/of';
 
 @Injectable()
 export class Service {
-  constructor() {}
+  private counter: number;
+  constructor() {
+    this.counter = 0;
+  }
 
   addFeed(action: any): Observable<any> {
-    return Observable.of('WAIT_NEEDED');
+    this.counter++;
+
+    if (this.counter > 3){
+      console.log('Loop counter: ' + this.counter);
+      console.log('It should stop after 3 iterations and terminate with the reducer to ADD + SUCCESS' );
+      return Observable.of('LOOP_SHOULD_BE_OVER');
+    } else
+      return Observable.of('WAIT_NEEDED');
   }
 
   waitForXSec(number: number): Observable<any> {
-    return Observable.of('');
+    return Observable.interval(number *1000); // let's wait x seconds before changing the state
   }
 }
